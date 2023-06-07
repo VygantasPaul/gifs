4e19b055d2262c9592ccd8a573492c4deeaa02c3
Vygantas Pauliukonis <wygantas.pauliukonis@gmail.com>
2023-06-07 13:03:38 +0300

    update

---
+++ build/precache-manifest.0e6b877a4ae1a88b891e925bd46105ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd39ddc8174ffd818b2a7bad4374d9f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "446897c292a3737cb708",
+    "url": "/static/css/main.87b910a0.chunk.css"
+  },
+  {
+    "revision": "04de3cc577c615346469",
+    "url": "/static/js/2.f2fe868b.chunk.js"
+  },
+  {
+    "revision": "fbb099047a7565816a67bf23043db0ec",
+    "url": "/static/js/2.f2fe868b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "446897c292a3737cb708",
+    "url": "/static/js/main.507ef64d.chunk.js"
+  },
+  {
+    "revision": "21864de6f4964ad1d4d0",
+    "url": "/static/js/runtime-main.e82b3059.js"
+  },
+  {
+    "revision": "acf88fd64f2ea057b29a971db8aa17b0",
+    "url": "/static/media/TESTHY.acf88fd6.svg"
+  }
+]);
